b881b65446cfcf4de382d6706e056207f0184447
Kaushal Patel <kaushalpatel922@gmail.com>
2016-06-16 23:50:40 -0400

    React Router Setup

--- app/app.jsx
+++ app/app.jsx
@@ -1,19 +1,16 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+//same as writing var Route = require('react-router').Route;
+var {Route, Router, IndexRoute, hashHistory} = require('react-router');
+var Main = require('Main');
+var Nav = require('Nav');
 
-var objOne = {
-  name: 'Kaushal',
-  location: 'Laurel'
-};
-
-var objTwo = {
-  age: 25,
-  ...objOne
-};
-
-console.log(objTwo);
 
 ReactDOM.render(
-  <h1>Boilerplate app!</h1>,
+  <Router history={hashHistory}>
+    <Route path="/" component={Main}>
+
+    </Route>
+  </Router>,
   document.getElementById('app')
 );
