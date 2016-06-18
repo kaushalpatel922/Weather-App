db7af5fb17d4ff98571cbc365685ffdede46d6c3
Kaushal Patel <kaushalpatel922@gmail.com>
2016-06-17 23:59:09 -0400

    fixed link with indexlink

--- public/bundle.js
+++ public/bundle.js
@@ -24828,6 +24828,7 @@
 	var _require = __webpack_require__(159);
 
 	var Link = _require.Link;
+	var IndexLink = _require.IndexLink;
 
 
 	var Nav = React.createClass({
@@ -24843,19 +24844,24 @@
 	        'Nav component'
 	      ),
 	      React.createElement(
-	        Link,
-	        { to: '/' },
+	        IndexLink,
+	        { to: '/', activeClassName: 'active', activeStyle: { fontWeight: 'bold' } },
 	        'Get Weather'
 	      ),
 	      React.createElement(
-	        Link,
-	        { to: '/about' },
+	        IndexLink,
+	        { to: '/about', activeClassName: 'active', activeStyle: { fontWeight: 'bold' } },
 	        'About'
 	      ),
 	      React.createElement(
-	        Link,
-	        { to: '/examples' },
+	        IndexLink,
+	        { to: '/examples', activeClassName: 'active', activeStyle: { fontWeight: 'bold' } },
 	        'Examples'
+	      ),
+	      React.createElement(
+	        'a',
+	        { href: '#/about' },
+	        'Go To About '
 	      )
 	    );
 	  }
