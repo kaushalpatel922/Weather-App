84232bcb4ac9f70d0f752166cb1bd6b376cb3487
Kaushal Patel <kaushalpatel922@gmail.com>
2016-06-19 11:01:11 -0400

    http & https fix

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 var express = require('express');
 
 //Create our app
-
 var app = express();
+// figures out the port or use 3000
+const PORT = process.env.PORT || 3000;
+
+// if sent to https - redirect to http
+app.use(function(req, res, next) {
+  if (req.header['x-forwarded-proto'] === 'http') {
+    next();
+  } else {
+    response.redirect('http://' + req.hostname + req.url)
+  }
+});
 
 app.use(express.static('public'));
 
-app.listen(3000, function() {
-  console.log("express server is up on port 3000");
+app.listen(PORT, function() {
+  console.log("express server is up on port port: " + PORT);
 });
