c2f9bfb14fdaf369b895f3d57968044e3641fda8
Kaushal Patel <kaushalpatel922@gmail.com>
2016-06-20 17:46:34 -0400

    added grid layout in example

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,10 +1,22 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var Examples = (props) => {
   return(
     <div>
-      <h3>Examples component</h3>
-      <p>Welcome to Examples!</p>
+      <h1 className="text-center">Examples</h1>
+      <p>Here are a few example locations.</p>
+      <ul>
+        <li>
+          <Link to="/?location=Laurel">Laurel, MD</Link>
+        </li>
+        <li>
+          <Link to="/?location=Washington">Washington, DC</Link>
+        </li>
+        <li>
+          <Link to="/?location=New York">New York, NY</Link>
+        </li>
+      </ul>
     </div>
   )
 };
